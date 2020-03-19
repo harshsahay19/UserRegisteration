496313255c82e60dc754fdb361d89ad481e354c2
745908 <745908@PC434355.cts.com>
2020-03-18 21:05:35 -0400

    First commit

---
+++ User_Course/src/com/harshsahay/registeration/model/UserRegisteration.java
@@ -0,0 +1,56 @@
+package com.harshsahay.registeration.model;
+
+public class UserRegisteration {
+	
+	private int User_ID;
+	private String User_First_Name;
+	private String User_Last_Name;
+	private String User_Email;
+	private String User_Password;
+	private int User_Mobile;
+	private String User_City;
+	public int getUser_ID() {
+		return User_ID;
+	}
+	public void setUser_ID(int user_ID) {
+		User_ID = user_ID;
+	}
+	public String getUser_First_Name() {
+		return User_First_Name;
+	}
+	public void setUser_First_Name(String user_First_Name) {
+		User_First_Name = user_First_Name;
+	}
+	public String getUser_Last_Name() {
+		return User_Last_Name;
+	}
+	public void setUser_Last_Name(String user_Last_Name) {
+		User_Last_Name = user_Last_Name;
+	}
+	public String getUser_Email() {
+		return User_Email;
+	}
+	public void setUser_Email(String user_Email) {
+		User_Email = user_Email;
+	}
+	public String getUser_Password() {
+		return User_Password;
+	}
+	public void setUser_Password(String user_Password) {
+		User_Password = user_Password;
+	}
+	public int getUser_Mobile() {
+		return User_Mobile;
+	}
+	public void setUser_Mobile(int user_Mobile) {
+		User_Mobile = user_Mobile;
+	}
+	public String getUser_City() {
+		return User_City;
+	}
+	public void setUser_City(String user_City) {
+		User_City = user_City;
+	}
+	
+
+}
